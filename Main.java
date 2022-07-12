febf9fd9ce3afc2dc3359d7be7db03c971530c22
Vishal Rankhamb <vishal.rankhamb@gmail.com>
2022-07-12 09:57:22 +0530

    1

--- Main.java
+++ Main.java
@@ -1,2 +1,9 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+    public static void main(String[] args){
+        System.out.println(4+5);
+
+        int a;
+        a=20;
+        System.out.println(a);
+    }
 }
